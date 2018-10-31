67a2008f552b5b1b72aa184c3a2e3ed3c4cc4f42
Aleksas <>
2018-10-31 20:52:51 +0800

    Reading points from file

--- kd-tree.py
+++ kd-tree.py
@@ -1,7 +1,12 @@
 file = open("..\Project2-ml\MLHW2\datasets\points.txt", "r")
 
 line = file.readline()
+points = []
 while line:
-    print(line)
+    line = line.split()
+    x = int(line[0])
+    y = int(line[1])
+    points.append({x, y})
     line = file.readline()
 
+print(points)
